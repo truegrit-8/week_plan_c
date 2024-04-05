bac45c36d6398ffb5b43afe270ae9e4dfdf8868b
truegrit-8 <truegrit8@163.com>
2024-04-05 19:09:10 +0800

    多文件编译成功

---
+++ inc/swap.h
@@ -0,0 +1,6 @@
+#ifndef __SWAP_H__
+#define __SWAP_H__
+
+void Swap(int *a, int *b);
+
+#endif
---
+++ src/swap.c
@@ -0,0 +1,8 @@
+#include "swap.h"
+
+void Swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
