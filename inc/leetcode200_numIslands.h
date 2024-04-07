abc908c1255ffaf0aad6e60d604531ff82745811
truegrit-8 <truegrit8@163.com>
2024-04-08 00:50:12 +0800

    200.岛屿数量，BFS解法，同名函数变量需要加static

--- inc/leetcode200_numIslands.h
+++ inc/leetcode200_numIslands.h
@@ -3,5 +3,7 @@
 
 void test_dfs_leetcode200_numIslands_case1(void);
 void test_dfs_leetcode200_numIslands_case2(void);
+void test_bfs_leetcode200_numIslands_case1(void);
+void test_bfs_leetcode200_numIslands_case2(void);
 
 #endif /* D8E02763_D9F8_45DC_A0F8_BBDAA772F570 */
--- src/bfs_leetcode200_numIslands.c
+++ src/bfs_leetcode200_numIslands.c
@@ -47,6 +47,7 @@ grid[i][j] 的值为 '0' 或 '1'
 // 优质题解：https://leetcode.cn/problems/number-of-islands/solutions/211211/dao-yu-lei-wen-ti-de-tong-yong-jie-fa-dfs2-bian-li-/
 
 int **visited;
+int **que;
 int rows;
 int cols;
 static int dir[4][2] = {{0, 1},    // 右
@@ -54,17 +55,66 @@ static int dir[4][2] = {{0, 1},    // 右
                         {-1, 0},   // 上
                         {0, -1}};  // 左
 
-static void dfs(char** grid, int x, int y)
+// BFS模版
+#if 0
+bool visited[rows][cols];
+void bfs(int sx, int sy)
 {
-    for (int i = 0; i < 4; i++) {
-        int nextx = x + dir[i][0];
-        int nexty = y + dir[i][1];
-        if (nextx < 0 || nextx >= rows || nexty < 0 || nexty >= cols) {
-            continue;  // 越界了，直接跳过
+    queue<pair<int, int>> que;
+    // step1. 源点入队
+    que.push({sx, sy});
+    visited[sx][sy] = true;
+
+    int dx[] = {0, 0, 1, -1};  // 下，上
+    int dy[] = {1, -1, 0, 0,}; // 右，左
+
+    // step2. 当队列不为空时，取出队首元素
+    while (!que.empty()) {
+        pair<int, int> cur = que.front();
+        que.pop();
+
+        // step3. 对队首元素进行扩展（遍历孩子）
+        int curx = cur.first;
+        int cury = cur.second;
+        for (int i = 0; i < 4; i++) {
+            int nx = curx + dx[i];
+            int ny = cury + dy[i];
+            if (nx > 0 && nx < rows && ny > 0 && ny < cols && !visited[nx][ny]) ) {
+                que.push({nx, ny});
+                visited[nx][ny] = true;
+            }
         }
-        if (visited[nextx][nexty] == 0 && grid[nextx][nexty] == '1') {  // 没有访问过的 同时 是陆地的
-            visited[nextx][nexty] = 1;
-            dfs(grid, nextx, nexty);
+    }
+}
+#endif
+
+static void bfs(char** grid, int x, int y)
+{
+    int left = 0;
+    int right = 0;
+    // push
+    que[right][0] = x;
+    que[right++][1] = y;
+    visited[x][y] = 1;
+
+    while (left < right) {
+        // pop
+        int curx = que[left][0];
+        int cury = que[left][1];
+        left++;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = curx + dir[i][0];
+            int ny = cury + dir[i][1];
+            if (nx < 0 || nx >= rows || ny < 0 || ny >= cols) {
+                continue;
+            }
+            if (visited[nx][ny] == 0 && grid[nx][ny] == '1') {
+                visited[nx][ny] = 1;
+                // push
+                que[right][0] = nx;
+                que[right++][1] = ny;
+            }
         }
     }
 }
@@ -74,6 +124,11 @@ static int numIslands(char** grid, int gridSize, int* gridColSize)
     int ans = 0;
     rows = gridSize;
     cols = gridColSize[0];
+    int nums = rows * cols;
+    que = (int **)malloc(sizeof(int *) * rows * cols);
+    for (int i = 0; i < nums; i++) {
+        que[i] = (int *)malloc(sizeof(int) * 2);
+    }
 
     // 二维数组初始化为零
     visited = (int **)malloc(sizeof(int *) * rows);
@@ -86,14 +141,14 @@ static int numIslands(char** grid, int gridSize, int* gridColSize)
         for (int j = 0; j < cols; j++) {
             if (visited[i][j] == 0 && grid[i][j] == '1') {
                 ans++;  // 遇到没访问过的陆地，+1
-                dfs(grid, i, j); // 将与其链接的陆地都标记上 true
+                bfs(grid, i, j); // 将与其链接的陆地都标记上 true
             }
         }
     }
     return ans;
 }
 
-void test_dfs_leetcode200_numIslands_case1(void)
+void test_bfs_leetcode200_numIslands_case1(void)
 {
     char grid[4][5] = {{'1', '1', '1', '1', '0'},
                        {'1', '1', '0', '1', '0'},
@@ -107,7 +162,7 @@ void test_dfs_leetcode200_numIslands_case1(void)
     printf("case1: ans = %d\n", ret);
 }
 
-void test_dfs_leetcode200_numIslands_case2(void)
+void test_bfs_leetcode200_numIslands_case2(void)
 {
     char grid[4][5] = {{'1', '1', '0', '0', '0'},
                        {'1', '1', '0', '0', '0'},
@@ -121,40 +176,3 @@ void test_dfs_leetcode200_numIslands_case2(void)
     printf("case2: ans = %d\n", ret);
 }
 
-// 版本一
-#if 0
-class Solution {
-   private:
-    int dir[4][2] = {0, 1, 1, 0, -1, 0, 0, -1};  // 四个方向
-    void dfs2(vector<vector<char>>& grid, vector<vector<bool>>& visited, int x, int y) {
-        for (int i = 0; i < 4; i++) {
-            int nextx = x + dir[i][0];
-            int nexty = y + dir[i][1];
-            if (nextx < 0 || nextx >= grid.size() || nexty < 0 || nexty >= grid[0].size()) {
-                continue;  // 越界了，直接跳过
-            }
-            if (!visited[nextx][nexty] && grid[nextx][nexty] == '1') {  // 没有访问过的 同时 是陆地的
-                visited[nextx][nexty] = true;
-                dfs2(grid, visited, nextx, nexty);
-            }
-        }
-    }
-
-   public:
-    int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<bool>> visited = vector<vector<bool>>(n, vector<bool>(m, false));
-
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    result++;  // 遇到没访问过的陆地，+1
-                    dfs2(grid, visited, i, j);  // 将与其链接的陆地都标记上 true
-                }
-            }
-        }
-        return result;
-    }
-};
-#endif
